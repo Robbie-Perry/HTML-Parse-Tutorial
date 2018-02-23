5f48ca73995d009b404acb8ae31a8c4cae5baeab
Robbie Perry <robbieperry22@gmail.com>
2018-02-22 16:34:08 -0800

    Initial

---
+++ HTML Parse/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  HTML Parse
+//
+//  Created by Robbie Perry on 2018-02-22.
+//  Copyright © 2018 Robbie Perry. All rights reserved.
+//
+
+import UIKit
+import TraceLog
+import Alamofire
+import SwiftSoup
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '10.3'
+
+target 'HTML Parse' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for HTML Parse
+  pod 'Alamofire', '~> 4.6'
+  pod 'TraceLog'
+  pod 'SwiftSoup'
+end
